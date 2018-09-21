c5cc1a33b3f8be6f2ae81d833dd3870b5d7b9dec
arqarq <43341071+arqarq@users.noreply.github.com>
2018-09-21 20:42:59 +0200

    modyfikacja paczek

--- src/main/java/zadanie10Izomorficzne/Klasa.java
+++ src/main/java/zadanie10Izomorficzne/Klasa.java
@@ -1,10 +1,10 @@
-package zadaniaProg1;
+package zadanie10Izomorficzne;
 
 import java.util.HashMap;
 import java.util.Map;
 
-class Zadanie10Izomorficzne {
-    private static boolean isIzomorf(String str1, String str2) {
+class Klasa {
+    static boolean isIzomorf(String str1, String str2) {
         char[] tabChar1 = str1.toCharArray();
         char[] tabChar2 = str2.toCharArray();
         Map<Character, Character> mapa = new HashMap<>();
@@ -21,18 +21,5 @@ class Zadanie10Izomorficzne {
             return false;
         }
         return true;
-    }
-
-    public static void main(String[] args) {
-        String str1 = "egggu";
-        String str2 = "adddt";
-
-        System.out.println(str1);
-        System.out.println(str2);
-        if (isIzomorf(str1, str2)) {
-            System.out.println("Są izomorficzne.");
-        } else {
-            System.out.println("Nie są izomorficzne.");
-        }
     }
 }
---
+++ src/main/java/zadanie10Izomorficzne/Runner.java
@@ -0,0 +1,18 @@
+package zadanie10Izomorficzne;
+
+import static zadanie10Izomorficzne.Klasa.isIzomorf;
+
+class Runner {
+    public static void main(String[] args) {
+        String str1 = "egggu";
+        String str2 = "adddt";
+
+        System.out.println(str1);
+        System.out.println(str2);
+        if (isIzomorf(str1, str2)) {
+            System.out.println("Są izomorficzne.");
+        } else {
+            System.out.println("Nie są izomorficzne.");
+        }
+    }
+}
