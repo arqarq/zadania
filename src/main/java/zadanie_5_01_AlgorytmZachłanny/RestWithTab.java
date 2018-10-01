6316dd96776387fa8fb786847fa174c8057a2dd5
arqarq <43341071+arqarq@users.noreply.github.com>
2018-10-01 12:21:21 +0200

    -zadanie 5_01: testy, zmiany wyświetlania

--- src/main/java/zadanie_5_01_AlgorytmZachłanny/RestWithTab.java
+++ src/main/java/zadanie_5_01_AlgorytmZachłanny/RestWithTab.java
@@ -1,9 +1,12 @@
 package zadanie_5_01_AlgorytmZachłanny;
 
+import java.util.Arrays;
+
 class RestWithTab {
+    private static final int[] nominals = {10, 5, 2, 1};
+    private static final int[] amounts = {12, 10, 11, 18};
+
     private static int greedy(int rest) {
-        int[] nominals = {10, 5, 2, 1};
-        int[] amounts = {12, 10, 11, 18};
         int c = 0;
 
         for (int i = 0; i < nominals.length; i++) {
@@ -12,9 +15,11 @@ class RestWithTab {
                 if (nominals[i] * amounts[i] >= rest) {
                     rest -= count * nominals[i];
                     c += count;
+                    amounts[i] -= count;
                 } else {
                     rest -= nominals[i] * amounts[i];
                     c += amounts[i];
+                    amounts[i] = 0;
                 }
             }
         }
@@ -25,11 +30,20 @@ class RestWithTab {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println(greedy(99));
-        System.out.println(greedy(100));
-        System.out.println(greedy(101));
-        System.out.println(greedy(210)); // max 51 monet
-        System.out.println(greedy(211));
+    public static void main(String[] args) { // start 51 monet
+        RestWithTab rest = new RestWithTab();
+        System.out.println(rest);
+        System.out.format("Wydano: " + greedy(99) + " monet dla reszty: %s%n", 99);
+        System.out.println(rest);
+        System.out.println(greedy(1));
+        System.out.println(rest);
+        System.out.println(greedy(110));
+        System.out.println(rest);
+        greedy(1000);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(nominals) + System.lineSeparator() + Arrays.toString(amounts);
     }
 }
